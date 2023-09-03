a83d53e82db1a01822b0e956b23a76afa5638c88
LastGregStanding <125837709+LastGregStanding@users.noreply.github.com>
2023-09-03 12:35:36 -0700

    added the code I had already done

---
+++ app.js
@@ -0,0 +1,44 @@
+"use strict";
+const info = document.querySelector("#poke-info");
+const pokemonImg = document.querySelector(".project-image");
+const btn = document.querySelector(".poke-btn");
+const name = document.querySelector(".name");
+const type = document.querySelector(".type");
+const height = document.querySelector(".height");
+
+btn.addEventListener("click", function () {
+  const pokemonSearch = document.querySelector("#search").value;
+  getPokemon(pokemonSearch);
+});
+
+const getPokemon = function (pokemon) {
+  const pokemonInfo = fetch(`https://pokeapi.co/api/v2/pokemon/${pokemon}`)
+    .then((res) => res.json())
+    .then((data) => {
+      console.log(data);
+      pokemonImg.src = data.sprites.front_default;
+      name.textContent = data.name;
+      type.textContent = data.types[0].type.name;
+      height.textContent = data.height + " ft";
+
+      // Erase the previous pokemon's abilities
+      const elementsToRemove = document.querySelectorAll(".ability");
+      elementsToRemove.forEach((element) => {
+        element.remove();
+      });
+
+      // Add the abilities
+      for (let i = 0; i < data.abilities.length; i++) {
+        let move = document.createElement("div");
+        move.classList.add("info");
+        move.classList.add("ability");
+        let abilities = document.createElement("h2");
+        abilities.textContent = `Ability ${i + 1}:`;
+        let abilityName = document.createElement("h2");
+        abilityName.textContent = data.abilities[i].ability.name;
+        move.appendChild(abilities);
+        move.appendChild(abilityName);
+        info.appendChild(move);
+      }
+    });
+};
